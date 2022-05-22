408d77abb9a86dcee05774332c4860c0b7c16dae
Nikhilhaspe <nikhilhaspe5220@gmail.com>
2022-05-22 13:24:38 +0530

    fix git pages

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
 
+console.log('ji');
+
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
