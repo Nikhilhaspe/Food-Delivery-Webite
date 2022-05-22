05f4c6d9a41525e627c8b0b9caffa3070b55e0d8
Nikhilhaspe <nikhilhaspe5220@gmail.com>
2022-05-22 12:50:18 +0530

    add loader while page is loading

--- js/script.js
+++ js/script.js
@@ -70,3 +70,14 @@ var swiper = new Swiper(".review-slider", {
     }
 });
 
+// loader scripts
+
+function loader() {
+    document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut() {
+    setInterval(loader, 3000);
+}
+
+window.onload = fadeOut;
