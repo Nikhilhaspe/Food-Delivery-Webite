dd165bc963705d1bd5359e9d9a1aa93c83d7c2dd
Nikhilhaspe <nikhilhaspe5220@gmail.com>
2022-05-22 12:40:14 +0530

    change active link in navbar as per scroll behaviour

--- js/script.js
+++ js/script.js
@@ -6,9 +6,27 @@ menu.onclick = () => {
     navbar.classList.toggle('active');
 }
 
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
 window.onscroll = () => {
+    // change active link of header as per the scroll
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop - 150;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header .navbar a[href *= ' + id + ']').classList.add('active');
+            });
+        }
+    });
 }
 
 document.querySelector('#search-icon').onclick = () => {
@@ -51,3 +69,4 @@ var swiper = new Swiper(".review-slider", {
         1024: { slidesPerView: 3, },
     }
 });
+
