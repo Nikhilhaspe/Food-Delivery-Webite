1a8d7392e0539d907794286e41bffe8f67e08ee1
Nikhilhaspe <60972722+Nikhilhaspe@users.noreply.github.com>
2022-06-20 22:07:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
 
-console.log('ji');
-
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
